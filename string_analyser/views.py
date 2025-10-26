90c8ca69a42d82f184a7efba1a4fb4d1272a8072
lawal tajudeen ogunsola <lwltjdn@gmail.com>
2025-10-26 16:31:40 +0100

    updated requirements

--- string_analyser/views.py
+++ string_analyser/views.py
@@ -1,5 +1,4 @@
 from rest_framework import status, generics
-from rest_framework import status, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.utils import timezone
